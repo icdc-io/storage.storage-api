39d37a27610e92b6cb52419a45426daae3a174bc
vstarastsenka@ibagroup.eu <vstarastsenka@ibagroup.eu>
2025-08-13 08:30:51 +0000

    Apply factory pattern

---
+++ app/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_cors import CORS
+from . import consts
+from .database import db, migrate
+from .loggers import log
+from .seed import seed as seed_data
+from .routers.account_router import account_management
+from .routers.health_check import health_check_bp
+from .routers.iscsi_router import iscsi
+from .routers.pools_router import pools
+from .routers.s3_router import s3
+
+
+def create_app():
+    """
+    Create and configure the Flask application.
+    """
+    app = Flask(__name__, template_folder="tmp")
+    # we can set level only after consts are imported
+    log.setLevel(consts.LOG_LEVEL)
+    db_conn = (
+        f"postgresql://"
+        f"{consts.DATABASE_USERNAME}:{consts.DATABASE_PASSWORD}@"
+        f"{consts.DATABASE_HOST}:{consts.DATABASE_PORT}/"
+        f"{consts.DATABASE_NAME}"
+    )
+
+    app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_conn
+    # app.config.from_object(config)
+
+    app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+
+    app.register_blueprint(health_check_bp)
+
+    app.register_blueprint(account_management, url_prefix="/api/v2/accounts")
+    app.register_blueprint(iscsi, url_prefix="/api/v2/iscsi")
+    app.register_blueprint(s3, url_prefix="/api/v2/s3")
+    app.register_blueprint(pools, url_prefix="/api/v2/pools")
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.cli.command("seed")(seed_data)
+
+    return app
--- app/database.py
+++ app/database.py
@@ -1,25 +1,15 @@
 """
 Define tables in Postgres database
 """
-import sys
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-from main import flask_app
-
-# Base declaration for SQLAlchemy
-Base = declarative_base()
-
-# Configure Flask app for SQLAlchemy
-db = SQLAlchemy(flask_app)
-
-migrate = Migrate(flask_app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 
 # Does not run seeding in migration context
-if len(sys.argv) < 2 or sys.argv[1] != 'db':
-    import app.seed as seed
-    # seed.seed(db.engine)
-    seed.seed()
+# if len(sys.argv) < 2 or sys.argv[1] != 'db':
+#     import app.seed as seed
+#     # seed.seed(db.engine)
+#     seed.seed()
